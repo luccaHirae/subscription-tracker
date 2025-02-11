1b11c1a0b0ce86de01d93d6b9685e585acf75a46
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-11 11:40:43 -0300

    chore: setup node env

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
 
+import { PORT } from "./config/env.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API");
 });
-
-const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,13 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      }  
+    },
+  },
   pluginJs.configs.recommended,
 ];
