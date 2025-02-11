f6ff63cf7176358bef7e0cca3ce81072fa8fadbb
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-11 11:26:42 -0300

    feat: add express server

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
