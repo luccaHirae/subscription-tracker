4ded02d6c60fb701695373d33bcfc1ce794ccea5
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-12 11:07:25 -0300

    feat: add error middleware

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
@@ -9,9 +11,15 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API");
