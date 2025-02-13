f697054ad6676a97f7d4f4c3b6723c28c83551f7
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-13 09:29:27 -0300

    feat: add upstash workflow

--- config/env.js
+++ config/env.js
@@ -3,11 +3,14 @@ import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const { 
-  PORT, 
+  PORT,
+  SERVER_URL,
   NODE_ENV, 
   DB_URI, 
   JWT_SECRET, 
   JET_EXPIRES_IN, 
   ARCJET_KEY,
   ARCJET_ENV,
+  QSTASH_URL,
+  QSTASH_TOKEN,
 } = process.env;
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,3 +1,5 @@
+import { SERVER_URL } from '../config/env.js';
+import { workflowClient } from '../config/upstash.js';
 import Subscription from '../models/subscription.model.js';
 
 export const createSubscription = async (req, res, next) => {
@@ -6,6 +8,10 @@ export const createSubscription = async (req, res, next) => {
       ...req.body,
       user: req.user._id,
     });
+
+    await workflowClient.trigger({
+      url: `${SERVER_URL}`
+    })
 
     res.status(201).json({
       success: true,
